ecdffa9e3cd55fa1b4e865453cc495c1bc924a49
zhangben <benzhang0416@163.com>
2021-06-24 23:01:50 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入全局样式表
+import '../src/assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
